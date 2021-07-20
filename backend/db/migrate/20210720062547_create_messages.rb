bd6c9f4f427ed8e2cb5d0592aadf431a842ac6f6
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-07-20 15:31:59 +0900

    マイグレーションファイル追記

--- backend/db/migrate/20210720062547_create_messages.rb
+++ backend/db/migrate/20210720062547_create_messages.rb
@@ -1,7 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[6.1]
   def change
     create_table :messages do |t|
-
+      t.references :user, index: true, foreign_key: true
+      t.string   :title,          null: false
+      t.string   :body,     null: false
+      t.string   :image
       t.timestamps
     end
   end
