f7844178ca4b5bc1c7c57a9c2f0374ecafaacf33
Yuma Tanaka <yumat9637@gmail.com>
2021-07-23 12:03:07 +0900

    Timezone設定
    外部への出力のTimezoneも設定

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -29,9 +29,9 @@ module Myapp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Tokyo'
     # config.eager_load_paths << Rails.root.join("extras")
-
+    config.active_record.default_timezone = :local
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
