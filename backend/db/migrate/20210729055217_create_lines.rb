a96a2d4370cbb8c9dd93bbaaecae0b1661f4c856
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-07-29 15:49:19 +0900

    マイグレーションファイル追記

--- backend/db/migrate/20210729055217_create_lines.rb
+++ backend/db/migrate/20210729055217_create_lines.rb
@@ -1,7 +1,9 @@
 class CreateLines < ActiveRecord::Migration[6.1]
   def change
     create_table :lines do |t|
-
+      t.references :uesr, index: true, foreign_key: true
+      t.string :original_id,     null: false
+      t.string :name,     null: false
       t.timestamps
     end
   end
---
+++ backend/db/migrate/20210729064348_create_chats.rb
@@ -0,0 +1,10 @@
+class CreateChats < ActiveRecord::Migration[6.1]
+  def change
+    create_table :chats do |t|
+      t.references :line, index: true, foreign_key: true
+      t.string :body,     null: false
+      t.string :send_flg,     null: false
+      t.timestamps
+    end
+  end
+end
