12499dd2550c61ba9c82a2c03a0458dbe64e3cb3
keisuke-333 <keikubo.999@gmail.com>
2021-07-29 13:42:25 +0900

    サイドバー項目変更

--- frontend/src/components/layouts/SideBar.tsx
+++ frontend/src/components/layouts/SideBar.tsx
@@ -1,17 +1,9 @@
-import React from 'react';
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import React from 'react'
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles'
+import { Drawer, CssBaseline, Toolbar, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
+import { Send, RecentActors } from '@material-ui/icons'
 
-const drawerWidth = 240;
+const drawerWidth = 240
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -33,11 +25,11 @@ const useStyles = makeStyles((theme: Theme) =>
       padding: theme.spacing(3),
     },
   }),
-);
+)
 
 type Props = {
-  isOpen: boolean;
-  handleSidebarToggle: VoidFunction;
+  isOpen: boolean
+  handleSidebarToggle: VoidFunction
 }
 
 const SideBar: React.FC<Props> = (props) => {
@@ -58,21 +50,14 @@ const SideBar: React.FC<Props> = (props) => {
         <Toolbar />
         <div className={classes.drawerContainer}>
           <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
+            <ListItem button >
+              <ListItemIcon><Send /></ListItemIcon>
+              <ListItemText primary="公式LINE投稿" />
+            </ListItem>
+            <ListItem button >
+              <ListItemIcon><RecentActors /></ListItemIcon>
+              <ListItemText primary="顧客管理" />
+            </ListItem>
           </List>
         </div>
       </Drawer>
