a900a48b0257bea69871504cb5767a990ff83d99
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-07-31 18:13:05 +0900

    追記

--- backend/app/controllers/api/v1/chats_controller.rb
+++ backend/app/controllers/api/v1/chats_controller.rb
@@ -1,2 +1,10 @@
-class Api::V1::ChatsController < ApplicationController
+class Api::V1::ChatsController < LineCommonsController
+  before_action :authenticate_api_v1_user!
+  def create
+    begin
+      
+    rescue => e
+      render json: { is_login: true, data: e }
+    end    
+  end
 end
