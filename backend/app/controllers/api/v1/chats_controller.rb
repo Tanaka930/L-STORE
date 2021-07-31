1c2445e6fe8cf983deb13dc09d39a89e16f4c608
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-07-31 18:45:46 +0900

    create作成

--- backend/app/controllers/api/v1/chats_controller.rb
+++ backend/app/controllers/api/v1/chats_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::ChatsController < LineCommonsController
   before_action :authenticate_api_v1_user!
   def create
     begin
-      
+      trg_line_user = LineCostmer.find(params[:line_costmer_id])
+      # params[:messate]は仮
+      # params[:image]は仮
+      insert(trg_line_user.id, params[:messate], params[:image], "0")
+      msg = "success"
+      render json: { is_login: true, data: msg }
     rescue => e
       render json: { is_login: true, data: e }
     end    
