f246f2cfe1f07bae87416e1f69001db527781f8d
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-08-02 16:03:07 +0900

    処理の分割

--- backend/app/controllers/api/v1/line_costmers_controller.rb
+++ backend/app/controllers/api/v1/line_costmers_controller.rb
@@ -17,57 +17,65 @@ class Api::V1::LineCostmersController < LineCommonsController
 
     # case event_type
     # when "follow"
-
     if event_type == "follow"
-      # lineのID取得
-      original_id = params[:events][0][:source][:userId]
-
-      # トークン情報を取得
-      token = Token.find_by(access_id: params[:token_access_id])
-
-      # トークンに紐づくユーザーを取得
-      user = User.find(token.user_id)
-
-      @url = "https://api.line.me/v2/bot/profile/#{original_id}"
-      @uri = URI.parse(@url)
-      @http = Net::HTTP.new(@uri.host,@uri.port)
-      @http.use_ssl = true
-      headers = {
-        'Authorization' => "Bearer #{token.messaging_token}",
-        # 'Content-Type' => 'application/json',
-        # 'Accept' => 'application/json'
-      }
-
-      response = @http.get(@uri.path, headers)
-      # binding.pry
-      case response
-      when Net::HTTPSuccess then
-        contact = JSON.parse(response.body)
-        costmer_name = contact['displayName']
-        costmer_image = contact['pictureUrl']
-        
-      else
-        p "#{response.code} #{response.body}"
-      end
-
-      # 新規作成の場合
-      @line_costmer = LineCostmer.new
-      @line_costmer.original_id = original_id
-      @line_costmer.user_id = user.id
-      @line_costmer.name = costmer_name
-      @line_costmer.image = costmer_image
-
-      if @line_costmer.save
-        render json: { status: 'SUCCESS', data: current_api_v1_user }
-        return
-      else
-        render json: { status: 'ERROR', data: current_api_v1_user }
-        return
-      end
+      follow()
     elsif event_type == "unfollow"
 
     elsif event_type == "message" or "image"
       resept_line_message(request)
     end
+  end
+
+  private
+  def follow()
+    # lineのID取得
+    original_id = params[:events][0][:source][:userId]
+
+    # トークン情報を取得
+    token = Token.find_by(access_id: params[:token_access_id])
+
+    # トークンに紐づくユーザーを取得
+    user = User.find(token.user_id)
+
+    @url = "https://api.line.me/v2/bot/profile/#{original_id}"
+    @uri = URI.parse(@url)
+    @http = Net::HTTP.new(@uri.host,@uri.port)
+    @http.use_ssl = true
+    headers = {
+      'Authorization' => "Bearer #{token.messaging_token}",
+      # 'Content-Type' => 'application/json',
+      # 'Accept' => 'application/json'
+    }
+
+    response = @http.get(@uri.path, headers)
+    # binding.pry
+    case response
+    when Net::HTTPSuccess then
+      contact = JSON.parse(response.body)
+      costmer_name = contact['displayName']
+      costmer_image = contact['pictureUrl']
+      
+    else
+      p "#{response.code} #{response.body}"
+    end
+
+    # 新規作成の場合
+    @line_costmer = LineCostmer.new
+    @line_costmer.original_id = original_id
+    @line_costmer.user_id = user.id
+    @line_costmer.name = costmer_name
+    @line_costmer.image = costmer_image
+
+    if @line_costmer.save
+      render json: { status: 'SUCCESS', data: current_api_v1_user }
+      return
+    else
+      render json: { status: 'ERROR', data: current_api_v1_user }
+      return
+    end
+  end
+
+  def unfollow()
+
   end
 end
