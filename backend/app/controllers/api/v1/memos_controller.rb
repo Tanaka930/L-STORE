af9bb84c031c840dbc4f46f6fbf92261505c3021
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-08-07 17:03:34 +0900

    memo_controllers追記

--- backend/app/controllers/api/v1/memos_controller.rb
+++ backend/app/controllers/api/v1/memos_controller.rb
@@ -1,6 +1,18 @@
 class Api::V1::MeMosController < ApplicationController
   before_action :authenticate_api_v1_user!
   def index
+    trg_user = LineCostmer.find(params[:line_costmer_id])
+    memos = Memo.where(line_costmer_id: trg_user.line_costmer_id)
+    render json: memos
+  end
+
+  def create
+    memos = Memo.create(memo_params)
+  end
+
+  private
+  def memo_params
+    params.require(:memos).permit(:line_costmer_id, :body)
   end
 
 end
