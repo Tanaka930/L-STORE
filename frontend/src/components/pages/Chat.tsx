6e92be5548106474995953762f7391f612462b1d
keisuke-333 <keikubo.999@gmail.com>
2021-08-09 18:58:07 +0900

    タブメニュー途中まで

--- frontend/src/components/pages/Chat.tsx
+++ frontend/src/components/pages/Chat.tsx
@@ -1,42 +1,8 @@
-import React, { useState ,useEffect, useContext } from "react"
-import { AuthContext } from "App"
-import axios from "axios"
-import Cookies from "js-cookie"
-import { Link } from "react-router-dom"
-import { CustomerList } from "interfaces/index"
-import { Chats } from "interfaces/index"
-import { makeStyles } from "@material-ui/core/styles"
-import { List, ListSubheader, ListItem, ListItemText, ListItemAvatar ,Avatar } from "@material-ui/core"
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    minWidth: 800
-  },
-  subhead: {
-    display: "flex",
-  },
-  subheadTitle: {
-    flex: "1"
-  }
-}))
-
 const Chat: React.FC = () => {
-  const classes = useStyles()
-  // const [customers, setCustomers] = useState<CustomerList[]>([])
-  const { currentUser } = useContext(AuthContext)
-  
-  const config = {
-    headers: {
-    "access-token": Cookies.get("_access_token"),
-    "client": Cookies.get("_client"),
-    "uid": Cookies.get("_uid")
-    }
-  }
-
-
   return (
-    <div className={classes.root}>
-    </div>
+    <>
+      <h1>トーク画面</h1>
+    </>
   )
 }
 
