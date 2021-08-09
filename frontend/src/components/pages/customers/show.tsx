a0c99d404430742b08747601216b13892d3fabcc
keisuke-333 <keikubo.999@gmail.com>
2021-08-09 13:21:11 +0900

    ユーザーID表示できるか確認

--- frontend/src/components/pages/customers/show.tsx
+++ frontend/src/components/pages/customers/show.tsx
@@ -1,8 +1,17 @@
-const CustomerDetail: React.FC = () => {
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
+const CustomerDetail: React.FC = () => {
+  const { id } = useParams<{ id: string }>()
+
+  useEffect(() => {
+    console.log(id)
+  }, [])
+  
   return (
     <>
       <h1>詳細画面</h1>
+      <p>ユーザーID：{ id }</p>
     </>
   )
 }
