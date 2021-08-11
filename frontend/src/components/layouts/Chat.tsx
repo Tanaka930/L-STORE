af905cfe95172d6f5a75604fb09bcc27eed3da6f
keisuke-333 <keikubo.999@gmail.com>
2021-08-11 16:15:38 +0900

    チャット文章表示されるかテスト

--- frontend/src/components/layouts/Chat.tsx
+++ frontend/src/components/layouts/Chat.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect, useContext } from "react"
+import { useState, useEffect } from "react"
+import Cookies from "js-cookie"
 import axios from "axios"
 
 type TabPanelProps = {
@@ -9,8 +10,28 @@ type TabPanelProps = {
 
 const Chat = (props: TabPanelProps) => {
   const { value, index, userId } = props
+  const [chats, setChats] = useState<any[]>([])
+
+  const getChats = async () => {
+    const config = {
+      headers: {
+        "access-token": Cookies.get("_access_token"),
+        "client": Cookies.get("_client"),
+        "uid": Cookies.get("_uid")
+      }
+    }
+
+    try {
+      const res = await axios.get(`${process.env.REACT_APP_API_URL}/line_customers/${userId}/chats`, config)
+      setChats(res.data)
+      console.log(res.data)
+    } catch(err) {
+      console.error(err.message)
+    }
+  }
 
   useEffect(() => {
+    getChats()
   }, [])
 
   return (
@@ -18,7 +39,10 @@ const Chat = (props: TabPanelProps) => {
       {value === index && (
         <>
           <h1>トーク画面</h1>
-          <p>ユーザーID：{userId}</p>
+          <h3>ユーザーID：{userId}</h3>
+          {chats.map((chat, index) => (
+            <p key={index}>チャット文章：{chat.body}</p>
+          ))}
         </>
       )}
     </>
