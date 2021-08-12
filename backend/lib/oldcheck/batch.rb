e670bd4bbe2952ddde382bf31aabd8c00174de21
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-08-12 15:19:56 +0900

    バッチファイル作成

--- backend/lib/oldcheck/batch.rb
+++ backend/lib/oldcheck/batch.rb
@@ -1,5 +1,13 @@
 module Oldcheck::Batch extend self
+  require "date"
   def batch
-    puts "テスト"
+    # 日付取得
+    d = Date.today
+    trg_datas = Chat.where("created_at <= ?", d-1)
+    ActiveRecord::Base.transaction do
+      trg_datas.each do |trg_data|
+        trg_data.destroy!
+      end
+    end
   end
 end
