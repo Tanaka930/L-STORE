f9e0d97a88f2999f47d4d72be514bf2d449fb68f
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-08-17 17:48:26 +0900

    マイグレーションファイル追加

--- backend/db/migrate/20210817083736_create_l_groups.rb
+++ backend/db/migrate/20210817083736_create_l_groups.rb
@@ -1,7 +1,8 @@
 class CreateLGroups < ActiveRecord::Migration[6.1]
   def change
     create_table :l_groups do |t|
-
+      t.references :user, index: true, foreign_key: true
+      t.string :name, null: false
       t.timestamps
     end
   end
--- backend/db/migrate/20210817083800_create_line_customer_l_groups.rb
+++ backend/db/migrate/20210817083800_create_line_customer_l_groups.rb
@@ -1,7 +1,8 @@
 class CreateLineCustomerLGroups < ActiveRecord::Migration[6.1]
   def change
     create_table :line_customer_l_groups do |t|
-
+      t.references :l_group, index: true, foreign_key: true
+      t.references :line_customer, index: true, foreign_key: true
       t.timestamps
     end
   end
