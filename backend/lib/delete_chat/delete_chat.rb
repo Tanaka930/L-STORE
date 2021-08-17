c1f358c1e476e9d403995dd34976f5d7a57f9c1b
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-08-17 14:20:40 +0900

    chat用のバッチファイル作成

--- backend/lib/delete_chat/delete_chat.rb
+++ backend/lib/delete_chat/delete_chat.rb
@@ -1,15 +1,14 @@
 module DeleteChat::DeleteChat extend self
+  # date型を取り扱うのでインポート
   require "date"
   def batch
     # 日付取得
     d = Date.today
     # タイムスタンプの場合は、00:00:00が基準
     # ターゲットの日付+1日で考える
-    trg_datas = Chat.where("created_at <= ?", d-89)
-    ActiveRecord::Base.transaction do
-      trg_datas.each do |trg_data|
-        trg_data.destroy
-      end
+    Chat.where("created_at <= ?", d+1).find_each do |trg_datas|
+      # 対象データを削除
+      trg_datas.destroy
     end
   end
 end
