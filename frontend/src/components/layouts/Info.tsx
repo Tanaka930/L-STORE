ec42c50617722f0a7a54a712bcbd237487d0e72c
keisuke-333 <keikubo.999@gmail.com>
2021-08-25 16:32:47 +0900

    アカウント情報画面途中まで

--- frontend/src/components/layouts/Info.tsx
+++ frontend/src/components/layouts/Info.tsx
@@ -2,7 +2,9 @@ import { useState, useEffect, useContext } from "react"
 import { AuthContext } from "App"
 import axios from 'axios'
 import Cookies from "js-cookie"
-import { Box, Container, Grid, Card, CardContent, CardHeader, TextField, MenuItem, InputLabel, Button, Divider } from "@material-ui/core"
+import { Box, Container, Grid, Card, CardContent, CardHeader, TextField, MenuItem, InputLabel, Button, Divider, IconButton, Typography } from "@material-ui/core"
+import SettingsIcon from '@material-ui/icons/Settings'
+import CloseIcon from '@material-ui/icons/Close'
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"
 
 type TabPanelProps = {
@@ -11,10 +13,26 @@ type TabPanelProps = {
   userId: string
 }
 
+type Info = {
+  lastName: string
+  firstName: string
+}
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
+      '& .MuiCardHeader-title': {
+        fontSize: 24,
+      },
+      '& .MuiCardContent-root': {
+        padding: 24,
+      },
+      '& .MuiCardHeader-root': {
+        textAlign: "center",
+        padding: 0,
+        paddingBottom: 9
+      },
     },
   }),
 )
@@ -34,7 +52,8 @@ const Info = (props: TabPanelProps) => {
   const [address, setAddress] = useState("")
   const [tel, setTel] = useState("")
   const [email, setEmail] = useState("")
-  const [customerInfo, setCustomerInfo] = useState([])
+  const [customerInfo, setCustomerInfo] = useState<Info[]>([])
+  const [edit, setEdit] = useState(false)
 
   const createFormData = (): FormData => {
     const formData = new FormData()
@@ -72,6 +91,10 @@ const Info = (props: TabPanelProps) => {
     }
   }
 
+  const handleToggleButton = () => {
+    setEdit(prevState => !prevState)
+  }
+
   const getCustomerInfo = async () => {
     try {
       const response = await axios.get(`${process.env.REACT_APP_API_URL}/tokens/${currentUser?.id}/line_customers/${userId}`, config)
@@ -107,186 +130,215 @@ const Info = (props: TabPanelProps) => {
       {value === index && (
         <Box py={3} className={classes.root}>
           <Container maxWidth="sm">
-            <form
-              noValidate
-              autoComplete="off"
-              onSubmit={handleInfoPost}
-            >
-              <Card>
-                <CardHeader
-                  title="アカウント情報"
-                  subheader="お客様情報を入力することができます"
-                />
-                <Divider variant="middle" />
+            <Card>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'flex-end',
+                  px: 1
+                }}
+              >
+                <IconButton onClick={handleToggleButton}>
+                  { edit ? <CloseIcon /> : <SettingsIcon /> }
+                </IconButton>
+              </Box>
+              <CardHeader
+                title="アカウント情報"
+                subheader={edit && "お客様情報をご入力ください"}
+              />
+              <Divider variant="middle" />
+              { edit ? (
+                <>
+                  <CardContent>
+                    <form
+                      noValidate
+                      autoComplete="off"
+                      onSubmit={handleInfoPost}
+                    >
+                      <Grid container spacing={3}>
+                        <Grid container item spacing={1}>
+                          <Grid item xs={12}>
+                            <InputLabel>名前</InputLabel>
+                          </Grid>
+                          <Grid item md={6} xs={12}>
+                            <TextField
+                              label="姓"
+                              variant="outlined"
+                              fullWidth
+                              value={lastName}
+                              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                setLastName(e.target.value)
+                              }}
+                            />
+                          </Grid>
+                          <Grid item md={6} xs={12}>
+                            <TextField
+                              label="名"
+                              variant="outlined"
+                              fullWidth
+                              value={firstName}
+                              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                setFirstName(e.target.value)
+                              }}
+                            />
+                          </Grid>
+                        </Grid>
+                        <Grid container item spacing={1}>
+                          <Grid item xs={12}>
+                            <InputLabel>生年月日</InputLabel>
+                          </Grid>
+                          <Grid item xs={4}>
+                            <TextField
+                              label="年"
+                              variant="outlined"
+                              fullWidth
+                              select
+                              value={year}
+                              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                setYear(e.target.value)
+                              }}
+                            >
+                              { years.map((year, index) => (
+                                <MenuItem key={index} value={year}>
+                                  {year}
+                                </MenuItem>
+                              ))}
+                            </TextField>
+                          </Grid>
+                          <Grid item xs={4}>
+                            <TextField
+                              id="month"
+                              label="月"
+                              variant="outlined"
+                              fullWidth
+                              select
+                              value={month}
+                              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                setMonth(e.target.value)
+                              }}
+                            >
+                              { months.map((month, index) => (
+                                <MenuItem key={index} value={month}>
+                                  {month}
+                                </MenuItem>
+                              ))}
+                            </TextField>
+                          </Grid>
+                          <Grid item xs={4}>
+                            <TextField
+                              label="日"
+                              variant="outlined"
+                              fullWidth
+                              select
+                              value={day}
+                              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                setDay(e.target.value)
+                              }}
+                            >
+                              { days.map((day, index) => (
+                                <MenuItem key={index} value={day}>
+                                  {day}
+                                </MenuItem>
+                              ))}
+                            </TextField>
+                          </Grid>
+                        </Grid>
+                        <Grid item xs={6}>
+                          <TextField
+                            label="年齢"
+                            variant="outlined"
+                            fullWidth
+                            select
+                            value={age}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                              setAge(e.target.value)
+                            }}
+                          >
+                            { ages.map((age, index) => (
+                              <MenuItem key={index} value={age}>
+                                {age}
+                              </MenuItem>
+                            ))}
+                          </TextField>
+                        </Grid>
+                        <Grid item xs={6}>
+                          <TextField
+                            label="性別"
+                            variant="outlined"
+                            fullWidth
+                            select
+                            value={sex}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                              setSex(e.target.value)
+                            }}
+                          >
+                            <MenuItem value="men">男</MenuItem>
+                            <MenuItem value="women">女</MenuItem>
+                          </TextField>
+                        </Grid>
+                        <Grid item xs={12}>
+                          <TextField
+                            label="住所"
+                            variant="outlined"
+                            fullWidth
+                            value={address}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                              setAddress(e.target.value)
+                            }}
+                          />
+                        </Grid>
+                        <Grid item md={6} xs={12}>
+                          <TextField
+                            label="電話番号"
+                            variant="outlined"
+                            helperText="ハイフン無しで入力してください"
+                            value={tel}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                              setTel(e.target.value)
+                            }}
+                          />
+                        </Grid>
+                        <Grid item xs={12}>
+                          <TextField
+                            label="メールアドレス"
+                            variant="outlined"
+                            fullWidth
+                            value={email}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                              setEmail(e.target.value)
+                            }}
+                          />
+                        </Grid>
+                        <Grid item xs={12}>
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            size="large"
+                            fullWidth
+                            type="submit"
+                          >
+                            保存
+                          </Button>
+                        </Grid>
+                      </Grid>
+                    </form>
+                  </CardContent>
+                </>
+              ) : (
                 <CardContent>
-                  <Grid container spacing={4}>
-                    <Grid container item spacing={1}>
-                      <Grid item xs={12}>
-                        <InputLabel>名前</InputLabel>
-                      </Grid>
-                      <Grid item md={6} xs={12}>
-                        <TextField
-                          label="姓"
-                          variant="outlined"
-                          fullWidth
-                          value={lastName}
-                          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            setLastName(e.target.value)
-                          }}
-                        />
-                      </Grid>
-                      <Grid item md={6} xs={12}>
-                        <TextField
-                          label="名"
-                          variant="outlined"
-                          fullWidth
-                          value={firstName}
-                          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            setFirstName(e.target.value)
-                          }}
-                        />
-                      </Grid>
-                    </Grid>
-                    <Grid container item spacing={1}>
-                      <Grid item xs={12}>
-                        <InputLabel>生年月日</InputLabel>
-                      </Grid>
-                      <Grid item xs={4}>
-                        <TextField
-                          label="年"
-                          variant="outlined"
-                          fullWidth
-                          select
-                          value={year}
-                          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            setYear(e.target.value)
-                          }}
-                        >
-                          { years.map((year, index) => (
-                            <MenuItem key={index} value={year}>
-                              {year}
-                            </MenuItem>
-                          ))}
-                        </TextField>
-                      </Grid>
-                      <Grid item xs={4}>
-                        <TextField
-                          id="month"
-                          label="月"
-                          variant="outlined"
-                          fullWidth
-                          select
-                          value={month}
-                          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            setMonth(e.target.value)
-                          }}
-                        >
-                          { months.map((month, index) => (
-                            <MenuItem key={index} value={month}>
-                              {month}
-                            </MenuItem>
-                          ))}
-                        </TextField>
-                      </Grid>
-                      <Grid item xs={4}>
-                        <TextField
-                          label="日"
-                          variant="outlined"
-                          fullWidth
-                          select
-                          value={day}
-                          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            setDay(e.target.value)
-                          }}
-                        >
-                          { days.map((day, index) => (
-                            <MenuItem key={index} value={day}>
-                              {day}
-                            </MenuItem>
-                          ))}
-                        </TextField>
-                      </Grid>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <TextField
-                        label="年齢"
-                        variant="outlined"
-                        fullWidth
-                        select
-                        value={age}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                          setAge(e.target.value)
-                        }}
+                  <Box sx={{p: 1}}>
+                    {/* {customerInfo.map((info, index) => (
+                      <Typography
+                        key={index}
+                        color="textPrimary"
                       >
-                        { ages.map((age, index) => (
-                          <MenuItem key={index} value={age}>
-                            {age}
-                          </MenuItem>
-                        ))}
-                      </TextField>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <TextField
-                        label="性別"
-                        variant="outlined"
-                        fullWidth
-                        select
-                        value={sex}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                          setSex(e.target.value)
-                        }}
-                      >
-                        <MenuItem value="men">男</MenuItem>
-                        <MenuItem value="women">女</MenuItem>
-                      </TextField>
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        label="住所"
-                        variant="outlined"
-                        fullWidth
-                        value={address}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                          setAddress(e.target.value)
-                        }}
-                      />
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                      <TextField
-                        label="電話番号"
-                        variant="outlined"
-                        helperText="ハイフン無しで入力してください"
-                        value={tel}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                          setTel(e.target.value)
-                        }}
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        label="メールアドレス"
-                        variant="outlined"
-                        fullWidth
-                        value={email}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                          setEmail(e.target.value)
-                        }}
-                      />
-                    </Grid>
-                    <Grid container item xs={12} justifyContent="flex-end">
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        size="large"
-                        type="submit"
-                      >
-                        保存
-                      </Button>
-                    </Grid>
-                  </Grid>
+                        {info.lastName}
+                      </Typography>
+                    ))} */}
+                  </Box>
                 </CardContent>
-              </Card>
-            </form>
+              )}
+            </Card>
           </Container>
         </Box>
       )}
