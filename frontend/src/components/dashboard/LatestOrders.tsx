3435e51ede70e97696b3ce74344b2b8dbd7a6bf3
Yuma Tanaka <yumat9637@gmail.com>
2021-08-27 11:21:58 +0900

    不要コード削除

--- frontend/src/components/dashboard/LatestOrders.tsx
+++ frontend/src/components/dashboard/LatestOrders.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 import moment from 'moment';
-import { v4 as uuid } from 'uuid';
+// import { v4 as uuid } from 'uuid';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import {
   Box,
@@ -21,79 +21,12 @@ import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 import { News, Contents } from "../../interfaces/index"
 import { microClient } from "../../lib/api/microClient";
 
-
-const orders = [
-  {
-    id: uuid(),
-    ref: 'CDD1049',
-    amount: 30.5,
-    customer: {
-      name: 'Ekaterina Tankova'
-    },
-    createdAt: 1555016400000,
-    status: 'pending'
-  },
-  {
-    id: uuid(),
-    ref: 'CDD1048',
-    amount: 25.1,
-    customer: {
-      name: 'Cao Yu'
-    },
-    createdAt: 1555016400000,
-    status: 'delivered'
-  },
-  {
-    id: uuid(),
-    ref: 'CDD1047',
-    amount: 10.99,
-    customer: {
-      name: 'Alexa Richardson'
-    },
-    createdAt: 1554930000000,
-    status: 'refunded'
-  },
-  {
-    id: uuid(),
-    ref: 'CDD1046',
-    amount: 96.43,
-    customer: {
-      name: 'Anje Keizer'
-    },
-    createdAt: 1554757200000,
-    status: 'pending'
-  },
-  {
-    id: uuid(),
-    ref: 'CDD1045',
-    amount: 32.54,
-    customer: {
-      name: 'Clarke Gillebert'
-    },
-    createdAt: 1554670800000,
-    status: 'delivered'
-  },
-  {
-    id: uuid(),
-    ref: 'CDD1044',
-    amount: 16.76,
-    customer: {
-      name: 'Adam Denisov'
-    },
-    createdAt: 1554670800000,
-    status: 'delivered'
-  }
-];
-
-
-
 export const LatestOrders = () => {
   const [news, setNews] = useState<News[]>([])
 
   const getNews = async () => {
     try {
       const data: Contents = await microClient.get({ endpoint: 'news' });
-      console.log(data.contents)
       setNews(data.contents)
     } catch(err) {
       console.error(err.message)
