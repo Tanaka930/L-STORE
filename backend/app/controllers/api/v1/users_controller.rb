38647cd700a98da2164fd22e6811958842937a56
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-09-02 13:30:04 +0900

    ルートを設定

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -56,6 +56,7 @@ class Api::V1::UsersController < ApplicationController
     end
     render json: json_data
   end
+  
   def get_follow_data
     # 最新のユーザーを1件取得
     # ここは後ほど修正
@@ -100,5 +101,51 @@ class Api::V1::UsersController < ApplicationController
     }
 
     render json: json_data
+  end
+
+  def create_subscription
+    begin
+      # Stripeのトークン
+      token = params[:stripeToken]
+
+      # ユーザ情報(メールアドレスなど一意なもの)
+      client = params[:client]
+
+      # 顧客の詳細情報
+      detail = params[:detail]
+
+      # 契約するプラン
+      plan = params[:plan]
+
+      # 顧客情報の作成
+      customer = Stripe::Customer.create(
+        :email => client,
+        :source => token,
+        :description => detail
+      )
+
+      # 作成された顧客のIDを取得
+      customer_id = customer.id
+
+      # Subsctiptionの作成
+      Stripe::Subscription.create(
+      customer: customer_id,
+      plan: plan
+      )
+
+      json_data = {
+        msg = "success"
+      }
+
+    rescue => e
+      # 例外が発生した際
+      json_data = {
+        msg = "error"
+        detail = e
+      }
+    end
+    
+    render json: json_data
+
   end
 end
