127610180af625f42b459cf22a036116ea70e937
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-09-02 12:34:41 +0900

    stripe用のライブラリ追加

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import StripeProvider from 'react-stripe-elements';
 
 ReactDOM.render(
   <React.StrictMode>
