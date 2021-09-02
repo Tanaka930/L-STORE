14557a47799fb8631cc369ddcc5a01b286a16d3a
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-09-02 15:44:41 +0900

    Update CheckOut.tsx

--- frontend/src/pages/CheckOut.tsx
+++ frontend/src/pages/CheckOut.tsx
@@ -4,9 +4,10 @@ import {Elements, StripeProvider} from 'react-stripe-elements';
 import CheckoutForm from 'components/ChackOutForms/CheckOutForm';
 
 const App: React.FC = () => {
+  const key: any = process.env.REACT_APP_STRIPE_KEY
   return (
     <div className="App">
-      <StripeProvider apiKey="pk_test_51JUq1hIrPhnpYWovSUw0dxeCGieBR1thicboDmm05hV0rHkuFD5IJMmgKFQjxBVDDUgZLorsmrcXWBTfPztDq6Kr00q8ZkRYKh">
+      <StripeProvider apiKey={key}>
         <Elements>
             <CheckoutForm />
         </Elements>
