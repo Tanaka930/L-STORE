c9026b64f5df5df3e5cd20286413957edef10e40
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-09-03 12:11:15 +0900

    マイグレーション

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_063357) do
+ActiveRecord::Schema.define(version: 2021_09_03_030016) do
 
   create_table "chatimages", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "chat_id"
@@ -149,6 +149,9 @@ ActiveRecord::Schema.define(version: 2021_08_26_063357) do
     t.text "tokens"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_credit_id"
+    t.string "encrypted_credit_id_iv"
+    t.string "encrypted_plan_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
