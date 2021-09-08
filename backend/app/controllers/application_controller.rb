c30441cfc8db87e9109c10b5f8eeb6aec8cfa38c
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-09-08 15:49:11 +0900

    edit

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -6,7 +6,10 @@ class ApplicationController < ActionController::Base
 
         def active_check
                 user = User.find(current_api_v1_user.id)
-                if user.subscription_status != "active"
+                # 現在時刻取得
+                now = Time.current
+                # サービスの状態がactiveではないもしくは、有効期限が切れている場合、サービスの機能をストップ
+                if user.subscription_status != "active" or user.service_expiration_date < now
                         render json: { error: 'forbidden' }, status: :forbidden
                 end
         end
