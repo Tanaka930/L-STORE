d13734ec9667e1d24f9d975b7b2e0357f5f0c28b
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-09-10 13:58:56 +0900

    edit

--- backend/app/controllers/api/v1/l_groups_controller.rb
+++ backend/app/controllers/api/v1/l_groups_controller.rb
@@ -1,12 +1,33 @@
 class Api::V1::LGroupsController < ApplicationController
   before_action :authenticate_api_v1_user!
   before_action :active_check
+  def index
+    group_names = GroupName.where(user_id, current_api_v1_user.id)
+    json_data = {
+      json:  {
+        "status" => 200,
+        "msg" => "succsess",
+        json_data => group_names
+      }
+    }
+
+    render json_data
+  end
+
   def create
     # groupの名前のパラメータ取得
     group_name = params[:group_name]
 
     # データをインサート
     insert(current_api_v1_user.id, group_name)
+
+    json_data = {
+      json:  {
+        "status" => 200,
+        "msg" => "succsess",
+      }
+    }
+    render json_data
   end
 
   private
