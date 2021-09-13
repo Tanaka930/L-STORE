559558ff008d1938f4b7296f120ec9ce19aefd41
Yuma Tanaka <yumat9637@gmail.com>
2021-09-13 16:16:05 +0900

    MenuTag

--- frontend/src/components/layouts/MenuTags.tsx
+++ frontend/src/components/layouts/MenuTags.tsx
@@ -4,52 +4,37 @@ import axios from 'axios'
 import moment from 'moment'
 import Cookies from "js-cookie"
 import { Box, Container, Card, CardContent, CardHeader, Paper, TextField, Button, Divider, Typography, IconButton} from "@material-ui/core"
-import PostAddIcon from '@material-ui/icons/PostAdd';
-import CloseIcon from '@material-ui/icons/Close'
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever'
-import EditIcon from '@material-ui/icons/Edit'
 import { Tag } from '../../interfaces/index'
-import clsx from 'clsx';
 import { createStyles, makeStyles, useTheme, Theme } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
-import ListItemText from '@material-ui/core/ListItemText';
 import Select from '@material-ui/core/Select';
-import Checkbox from '@material-ui/core/Checkbox';
-import { AnyNaptrRecord } from "dns"
-
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    root: {
+      flexGrow: 1,
+      '& .MuiCardHeader-title': {
+        fontSize: 24,
+      },
+      '& .MuiCardHeader-root': {
+        textAlign: "center",
+        padding: 0,
+        paddingBottom: 9
+      },
+      '& .MuiPaper-root': {
+        marginBottom: 24
+      }
+    },
     formControl: {
       margin: theme.spacing(1),
       minWidth: 120,
-      maxWidth: 300,
     },
-    chips: {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-    chip: {
-      margin: 2,
-    },
-    noLabel: {
-      marginTop: theme.spacing(3),
+    selectEmpty: {
+      marginTop: theme.spacing(2),
     },
   }),
-);
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
+)
 
 const names = [
   'Oliver Hansen',
@@ -64,14 +49,14 @@ const names = [
   'Kelly Snyder',
 ];
 
-function getStyles(name: string, personName: string[], theme: Theme) {
-  return {
-    fontWeight:
-      personName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
-  };
-}
+// function getStyles(name: string, personName: string[], theme: Theme) {
+//   return {
+//     fontWeight:
+//       personName.indexOf(name) === -1
+//         ? theme.typography.fontWeightRegular
+//         : theme.typography.fontWeightMedium,
+//   };
+// }
 
 type TabPanelProps = {
   index: number
@@ -84,20 +69,12 @@ const MenuTags = (props: TabPanelProps) => {
   // const [edit, setEdit] = useState(false)
   const [tags, setTags] =useState<any[]>([])
   const classes = useStyles();
-  const theme = useTheme();
-  const [personName, setPersonName] = useState<string[]>([]);
+  const [personName, setPersonName] = useState('');
 
-  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setPersonName(event.target.value as string[]);
+  const handleChange = (event: React.ChangeEvent<{ personName?: string | undefined; value: unknown; }>) => {
+    console.log(event.target.personName)
+    setPersonName(event.target.personName as string);
   };
-
-
-
-type TabPanelProps = {
-  index: number
-  value: number
-  userId: string
-}
 
   const config = {
     headers: {
@@ -119,18 +96,24 @@ type TabPanelProps = {
     }
   }
 
-  const onSubmit = async (value: any) => {
+  console.log(personName)
+
+  const onSubmit = async (personName: any) => {
     // ↓↓↓↓↓↓↓↓↓ 新規投稿処理 ↓↓↓↓↓↓↓↓↓
     try {
+      console.log('onSubmit')
       const response = await axios.post(`${process.env.REACT_APP_API_URL}/line_customers/${userId}/line_customer_l_groups
-      `, value, config)
+      `, personName, config)
       if (response.status === 200) {
-        getTags()
+        // getTags()
+        console.log('post')
         // toast.success("メモを投稿しました")
       } else {
+        console.log('not-post')
         // toast.error("投稿に失敗しました")
       }
     } catch(err) {
+
       // toast.warn("通信に失敗しました")
       console.error(err)
     }
@@ -142,26 +125,34 @@ type TabPanelProps = {
 
   return (
     <>
-      <form autoComplete="off" onSubmit={onSubmit}>
-      <FormControl variant="outlined" className={classes.formControl} >
-      <InputLabel id="demo-simple-select-outlined-label">タグ</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select-outlined"
-          value={value}
-          onChange={handleChange}
-          label="Tag"
-        >
-          {/* <MenuItem value="">
-            <em>None</em>
-          </MenuItem> */}
-          {tags.map((tag: Tag) => (
-            <MenuItem value={tag.groupId}>{tag.groupName}</MenuItem>
-          ))}
-        </Select>
-        <Button type="submit">submit</Button>
-      </FormControl>
-      </form>
+      {value === index && (
+        <Box py={3} className={classes.root}>
+          <Container maxWidth="sm">
+            <Card>
+            <form autoComplete="off" onSubmit={onSubmit}>
+              <FormControl variant="outlined" className={classes.formControl} >
+              <InputLabel id="demo-simple-select-outlined-label">タグ</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select-outlined"
+                  value={personName}
+                  onChange={handleChange}
+                  label="Tag"
+                >
+                  {/* <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem> */}
+                  {tags.map((tag: Tag) => (
+                    <MenuItem value={tag.groupId}>{tag.groupName}</MenuItem>
+                  ))}
+                </Select>
+                <Button type="submit">submit</Button>
+              </FormControl>
+            </form>
+          </Card>
+        </Container>
+      </Box>
+      )}
     </>
   )
 }
