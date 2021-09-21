12c6b22038809c2bffe6a34b39273d80143b7624
keisuke-333 <keikubo.999@gmail.com>
2021-09-21 15:28:17 +0900

    修正

--- frontend/src/lib/api/tag.ts
+++ frontend/src/lib/api/tag.ts
@@ -31,7 +31,6 @@ export const getTags = async (setTags: React.Dispatch<React.SetStateAction<GetTa
     const res = await client.get('l_groups', config)
     if (res.status === 200) {
       setTags(res.data.groupNameList)
-      console.log(res.data.groupNameList)
     }
   } catch(err) {
     console.error(err)
