a63521f02ecd3a1a5c7ec91110118885d4bbee1e
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-09-23 14:26:28 +0900

    edit

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 
       get '/line_customer/:user_id/search/group/:l_group_id', to: 'line_customers#tag_search_customer'
 
-      get '/users/:id/whebhook', to: 'tokens#get_webhook_url'
+      get '/users/:id/webhook', to: 'tokens#get_webhook_url'
 
       post '/users/:id/create_subscription', to: 'stripes#create_subscription'
 
